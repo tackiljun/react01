b9420060331520aa4608710d9e6ade00a055adb6
tarkIlJun <tack89826@gmail.com>
2023-09-19 18:50:16 +0900

    ReadPage

---
+++ src/pages/products/ReadPage.js
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom"
+import ReadComponent from "../../components/product/ReadComponent"
+import useCustomLogin from "../../hooks/useCustomLogin"
+import useQueryObj from "../../hooks/useQueryObj"
+
+
+const ReadPage = () => {
+
+    const {queryObj, setSearch, moveRead, moveList, moveModify} = useQueryObj()
+    // setSearch, moveRead, 안에 들어가?
+    const {pno} = useParams()
+
+    useCustomLogin(() => {
+        alert("로그인을 부탁드려요.....")
+    })
+
+    console.log(pno)
+    console.log(useQueryObj)
+    console.log("------------------------")
+
+    return ( 
+
+        <div>
+            <ReadComponent 
+            pno={pno} 
+            moveModify={moveModify} 
+            moveList={moveList}>
+            </ReadComponent>
+        </div>
+
+     );
+     
+}
+ 
+export default ReadPage;
