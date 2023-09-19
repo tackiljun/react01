cd580fb926c2f70bce33990017e139bdd1e0311a
tarkIlJun <tack89826@gmail.com>
2023-09-19 18:49:13 +0900

    ModifyPage

---
+++ src/pages/products/ModifyPage.js
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom"
+import ModifyComponent from "../../components/product/ModifyComponent"
+import useCustomLogin from "../../hooks/useCustomLogin"
+import useQueryObj from "../../hooks/useQueryObj"
+
+
+const ModifyPage = () => {
+
+    const {loginInfo} = useCustomLogin( (nav) => {
+
+        //console.log("function.....")
+        nav('.../list')
+
+    })
+
+    const {queryObj, setSearch, moveRead, moveList, moveModify} = useQueryObj()
+
+    // setSearch, moveRead, 안에 들어가?
+    const {pno} = useParams()
+
+    return ( 
+
+        <div>
+            <div>
+                PRODUCT MODIFY PAGE {pno}
+            </div>
+
+            <ModifyComponent 
+            pno={pno} 
+            moveList={moveList} 
+            moveRead={moveRead}>
+            </ModifyComponent>
+        </div>
+
+     );
+     
+}
+ 
+export default ModifyPage;
