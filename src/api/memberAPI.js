ec17799b406b9f3fd4691bcccd905a386cb196d5
tarkIlJun <tack89826@gmail.com>
2023-09-20 15:22:45 +0900

    memberAPI

--- src/api/memberAPI.js
+++ src/api/memberAPI.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 
+///////////////////////////////////////////////////////////////////////////////////
 export const postLogin = async(params) => {
 
     const res = await axios.post('http://localhost:8080/api/member/login', params)
