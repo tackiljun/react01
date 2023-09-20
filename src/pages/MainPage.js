1540a11eda5b15859e4f455f04ee948544e3bd7a
tarkIlJun <tack89826@gmail.com>
2023-09-20 15:10:57 +0900

    MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,7 +3,7 @@ import BasicLayout from "../layouts/BasicLayout";
 
 const MainPage = () => {
 
-    console.log("--------------- MAIN ---------------")
+    console.log("---------- MAIN ----------")
 
     return ( 
 
