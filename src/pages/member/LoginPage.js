3548d33319698a1d16cb64f6a890666464f1edcc
tarkIlJun <tack89826@gmail.com>
2023-09-21 20:26:57 +0900

    member-LoginPage

--- src/pages/member/LoginPage.js
+++ src/pages/member/LoginPage.js
@@ -3,8 +3,10 @@ import LoginComponent from "../../components/member/LoginComponent";
 import BasicLayout from "../../layouts/BasicLayout";
 
 
+////////////////////////////////////////////////////////
 const LoginPage = () => {
     
+////////////////////////////////////////////////////////    
     return ( 
 
         <BasicLayout>
