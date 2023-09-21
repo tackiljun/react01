f41d06a23d1929b7a5129c2601597231f0c4138a
tarkIlJun <tack89826@gmail.com>
2023-09-21 20:24:13 +0900

    board-ListPage

--- src/pages/board/ListPage.js
+++ src/pages/board/ListPage.js
@@ -3,6 +3,7 @@ import ListSearchComponent from "../../components/board/ListSearchComponent"
 import useQueryObj from "../../hooks/useQueryObj"
 
 
+//////////////////////////////////////////////////////////////////////////////
 // const checkNull = (obj) => {
 
 //     const result = {}
@@ -20,6 +21,7 @@ import useQueryObj from "../../hooks/useQueryObj"
 // }
 
 
+//////////////////////////////////////////////////////////////////////////////
 const ListPage = () => {
 
     const {queryObj, setSearch, moveRead, moveList} = useQueryObj()
@@ -28,23 +30,20 @@ const ListPage = () => {
     console.log(queryObj)
   
     const movePage = (num) => {
-  
         console.log("NUM ------------" + num)
         queryObj.page = num
         setSearch({...queryObj})
-
     }
   
     const moveSearch = (type, keyword) => {
-
         queryObj.page = 1
         queryObj.type = type
         queryObj.keyword = keyword
     
         setSearch({...queryObj})
-
     }
 
+//////////////////////////////////////////////////////////////////////////////
     return ( 
 
         <div>
@@ -58,12 +57,12 @@ const ListPage = () => {
             <ListComponent 
             queryObj={queryObj} 
             movePage = {movePage} 
-            moveRead = {moveRead}
-            >
+            moveRead = {moveRead}>
             </ListComponent>
         </div>
 
      );
+
 }
  
 export default ListPage;
