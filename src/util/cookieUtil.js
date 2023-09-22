da84d8c0c5830ef8e4f946c8f10aa01a2bb09175
tarkIlJun <tack89826@gmail.com>
2023-09-22 11:20:33 +0900

    cookieUtil

--- src/util/cookieUtil.js
+++ src/util/cookieUtil.js
@@ -1,6 +1,7 @@
 import { Cookies } from "react-cookie";
 
 
+//////////////////////////////////////////////////////////////////////////
 // Cookie는 문자열만 사용 가능.
 // JSON.parse(문자열) : JSON 문자열을 JavaScript 값이나 객체 변환.
 // JSON.stringify(JSON데이터) : JavaScript 값이나 객체를 JSON 문자열로 변환.
@@ -9,6 +10,7 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies()
 
 
+//////////////////////////////////////////////////////////////////////////
 export const setCookie = (cookieName, value, days) => {
 
     const expires = new Date()
@@ -18,17 +20,22 @@ export const setCookie = (cookieName, value, days) => {
     // 쿠키를 저장하면 옵션이 들어가.
     cookies.set(cookieName, value, { path: "/", expires: expires})
     // 로그인을 하면 쿠키값을 저장해야해.
+    
 }
 
 
+//////////////////////////////////////////////////////////////////////////
 export const getCookie = (cookieName) => {
 
     // 문자열.
     return cookies.get(cookieName)
+
 }
 
 
+//////////////////////////////////////////////////////////////////////////
 export const removeCookie = (cookieName, path="/") => {
 
     cookies.remove(cookieName, {path:path})
+
 }
