1f862455ebbd3729016233894d11fce8f96faacc
tarkIlJun <tack89826@gmail.com>
2023-10-09 16:09:36 +0900

    board-ReadComponent

--- src/components/board/ReadComponent.js
+++ src/components/board/ReadComponent.js
@@ -30,16 +30,16 @@ const ReadComponent = ({bno}) => {
         <table className="min-w-[1280px] ml-auto mr-auto">
             <thead>
                 <tr className="h-12 bg-blue-300">
-                    <th className="border-2 font-bold m-2" style={{ width: '5%' }}>
+                    <th className="border-2 font-bold m-2" style={{width: '5%'}}>
                         NO.
                     </th>
-                    <th className="border-2 font-bold m-2" style={{ width: '70%' }}>
+                    <th className="border-2 font-bold m-2" style={{width: '70%'}}>
                         TITLE
                     </th>
-                    <th className="border-2 font-bold m-2" style={{ width: '10%' }}>
+                    <th className="border-2 font-bold m-2" style={{width: '10%'}}>
                         WRITER
                     </th>
-                    <th className="border-2 font-bold m-2" style={{ width: '15%' }}>
+                    <th className="border-2 font-bold m-2" style={{width: '15%'}}>
                         REGDATE
                     </th>
                 </tr>
